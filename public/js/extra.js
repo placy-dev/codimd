92cdc9484b0154501c88a4da7cb7efa30cdfe9dc
Yukai Huang <yukaihuangtw@gmail.com>
2020-05-01 12:14:59 +0800

    Implement cspreview renderer
    Signed-off-by: Yukai Huang <yukaihuangtw@gmail.com>

--- public/js/extra.js
+++ public/js/extra.js
@@ -16,6 +16,7 @@ import { stripTags } from '../../utils/string'
 import getUIElements from './lib/editor/ui-elements'
 import { emojifyImageDir } from './lib/editor/constants'
 import { parseFenceCodeParams, serializeParamToAttribute } from './lib/markdown/utils'
+import { renderCSVPreview } from './lib/renderer/csvpreview'
 
 import markdownit from 'markdown-it'
 import markdownitContainer from 'markdown-it-container'
@@ -1167,6 +1168,12 @@ md.renderer.rules.fence = (tokens, idx, options, env, self) => {
 
   if (info) {
     langName = info.split(/\s+/g)[0]
+
+    if (langName === 'csvpreview') {
+      const params = parseFenceCodeParams(info)
+      return renderCSVPreview(token.content, params)
+    }
+
     if (/!$/.test(info)) token.attrJoin('class', 'wrap')
     token.attrJoin('class', options.langPrefix + langName.replace(/=$|=\d+$|=\+$|!$|=!$/, ''))
     token.attrJoin('class', 'hljs')
